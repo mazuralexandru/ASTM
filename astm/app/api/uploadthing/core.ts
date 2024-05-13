2d85e852d15a4d87883b5f575339100ee0e49540
Alexandru Mazur <alexandru.mazur@code11.com>
2024-05-13 03:20:46 +0300

    uploadThing setup

---
+++ astm/app/api/uploadthing/core.ts
@@ -0,0 +1,23 @@
+import { auth } from "@clerk/nextjs/server";
+
+const handleAuth = () => {
+  const { userId } = auth();
+  if (!userId) throw new Error("Unauthorized");
+  return { userId: userId };
+};
+
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
